600abb6128b8c077fbdfb01341c3bd32381792d5
Manni Reis <90857553+mannireis@users.noreply.github.com>
2024-01-04 16:59:19 +0000

    New everything added, hermitcraft wiki to be writen|Please contribute, My game

--- js.js
+++
@@ -1,8 +0,0 @@
-function makeRGB() {
-  const currentHue = document.documentElement.style.getPropertyValue("--hue");
-  document.documentElement.style.setProperty("--hue", (currentHue % 360) + 1);
-
-  setTimeout(makeRGB, 10);
-}
-
-makeRGB();
